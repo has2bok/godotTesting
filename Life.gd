af78fe7cc4dc2f5f69338ff7b3df66e8df733bb1
Has2bok <has2bok@yahoo.co.uk>
2018-08-15 20:26:29 +0100

    added more

--- Life.gd
+++ Life.gd
@@ -75,12 +75,24 @@ func drawLives():
 	#return world
 	
 func get_input():
+	var speed =10
+	if Input.is_action_pressed('ui_down'):
+		$lifeCamera2D.position.y += speed
+	if Input.is_action_pressed('ui_up'):
+		$lifeCamera2D.position.y -= speed
+	if Input.is_action_pressed('ui_left'):
+		$lifeCamera2D.position.x-=speed
+	if Input.is_action_pressed('ui_right'):
+        $lifeCamera2D.position.x+=speed
 	if Input.is_action_pressed('ui_page_up'):
-		print("Zoom up to "+str($Camera2D.zoom))
+		print("Zoom up to "+str($lifeCamera2D.zoom))
 		$lifeCamera2D.zoom-=Vector2(0.1,0.1)
 		print("page up pressed")
 	if Input.is_action_pressed('ui_page_down'):
 		$lifeCamera2D.zoom+=Vector2(0.1,0.1)
-		print("Zoom down to "+str($Camera2D.zoom))
+		print("Zoom down to "+str($lifeCamera2D.zoom))
 		print("page down pressed")
+
+func _physics_process(delta):
+    get_input()
 	
